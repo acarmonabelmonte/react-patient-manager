8dd6ec293f1cc4b35bebde07a7d434bb4bb6273c
Adrián Carmona Belmonte <acarmona.dev@gmail.com>
2023-10-28 13:17:35 +0200

    💄  styled PatientList

--- src/components/PatientForm.jsx
+++ src/components/PatientForm.jsx
@@ -1,6 +1,6 @@
 const PatientForm = () => {
   return (
-    <div className="md:w-1/2 lg:w-2/5">
+    <div className="w-full md:w-1/2 lg:w-2/5">
       <h2 className="font-black text-3xl text-center">Seguimiento Pacientes</h2>
       <p className="mt-5 text-lg text-center mb-10">
         Añade Pacientes y{" "}
--- src/components/PatientsList.jsx
+++ src/components/PatientsList.jsx
@@ -1,7 +1,19 @@
+import Patient from "./Patient";
+
 const PatientsList = () => {
   return (
-    <div className="md:w-1/2 lg:w-3/5">
-      <h2>Listado</h2>
+    <div className="w-full md:w-1/2 lg:w-3/5 md:h-screen overflow-y-scroll">
+      <h2 className="font-black text-3xl text-center">Listado Pacientes</h2>
+      <p className="text-xl mt-5 mb-10 text-center">
+        Administra tus {""}
+        <span className="bg-gradient-to-r from-blue-500 to-blue-600 text-transparent bg-clip-text font-bold">
+          Pacientes y Citas
+        </span>
+      </p>
+      <Patient />
+      <Patient />
+      <Patient />
+      <Patient />
     </div>
   );
 };
